e0582dd3e8cf82d887305693561bf2d62a0b4ce1
jorge <jorgepalen93@gmail.com>
2025-04-23 11:44:53 +0200

    cambio de rama

--- app/src/main/java/com/example/tfg_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/tfg_kotlin/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.tfg_kotlin
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +20,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_drag, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                return true
+            }
+            R.id.action_add_image -> {
+                Toast.makeText(this, "Añadir imagen pulsado", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_add -> {
+                Toast.makeText(this, "Añadir elemento pulsado", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
