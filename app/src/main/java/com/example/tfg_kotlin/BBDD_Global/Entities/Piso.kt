a1c5a83a2f6548edd5fa14a168cf53894478c272
jorge <jorgepalen93@gmail.com>
2025-06-07 10:57:08 +0200

    cambios en reservas

--- app/src/main/java/com/example/tfg_kotlin/BBDD_Global/Entities/Piso.kt
+++ app/src/main/java/com/example/tfg_kotlin/BBDD_Global/Entities/Piso.kt
@@ -2,7 +2,7 @@ package com.example.tfg_kotlin.BBDD_Global.Entities
 
 
 data class Piso(
-    var id: String? = null,           // Para guardar el ID del documento Firestore (opcional)
+    var id: String? = null,
     var nombre: String = "",
     var empresaCif: String = "",
     var imagenUrl: String? = null     // URL de la imagen almacenada en Firebase Storage
