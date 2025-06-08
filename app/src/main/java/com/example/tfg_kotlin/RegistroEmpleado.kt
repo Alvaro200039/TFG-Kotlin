9d44dc836abbeecfef3f1dbf02eac5166c5a73b0
Alvaro <alvar200039@gmail.com>
2025-06-08 13:30:46 +0200

    Comentado toda la parte iniaial (reguistros, login, recuperecion contraseña)

--- app/src/main/java/com/example/tfg_kotlin/RegistroEmpleado.kt
+++ app/src/main/java/com/example/tfg_kotlin/RegistroEmpleado.kt
@@ -15,6 +15,8 @@ import com.google.firebase.ktx.Firebase
 import androidx.appcompat.widget.Toolbar
 
 class RegistroEmpleado : AppCompatActivity() {
+
+    // Creación de variables globales
     private lateinit var etCorreo: EditText
     private lateinit var etContrasena: EditText
     private lateinit var etRepetirContrasena: EditText
@@ -23,6 +25,7 @@ class RegistroEmpleado : AppCompatActivity() {
     private lateinit var etCif: EditText
     private lateinit var btnRegistrar: Button
 
+    // Variable global para acceder a firebase
     private lateinit var auth: FirebaseAuth
     private val firestore = Firebase.firestore
 
@@ -37,13 +40,14 @@ class RegistroEmpleado : AppCompatActivity() {
             insets
         }
 
-        // Toolbar
+        // Toolbar con funcionalidad, nombre e icono
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = ""
+        supportActionBar?.title = "Registro de Empleados"
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_adagora)
 
+        // Hacemos referencia a los editText y los renombramos para el uso
         etCorreo = findViewById(R.id.etCorreo)
         etCif = findViewById(R.id.etCif)
         etNombre = findViewById(R.id.etNombre)
@@ -52,14 +56,18 @@ class RegistroEmpleado : AppCompatActivity() {
         etRepetirContrasena = findViewById(R.id.etRepetirContrasena)
         btnRegistrar = findViewById(R.id.btnRegistrar)
 
+        // Instanciamos firebase para su uso
         auth = FirebaseAuth.getInstance()
 
+        // Botón para registrar el empleado
         btnRegistrar.setOnClickListener {
             registrarEmpleado()
         }
     }
 
     private fun registrarEmpleado() {
+
+        // Variable local de la función pare hacer uso de los detos de los editText
         val correo = etCorreo.text.toString().trim()
         val cif = etCif.text.toString().trim()
         val contrasena = etContrasena.text.toString()
@@ -67,36 +75,47 @@ class RegistroEmpleado : AppCompatActivity() {
         val nombre = etNombre.text.toString().trim()
         val apellidos = etApellidos.text.toString().trim()
 
+        //Validación de que todos los campos obligatorios están introdicidos
         if (correo.isEmpty() || contrasena.isEmpty() || repetirContrasena.isEmpty() || nombre.isEmpty() || apellidos.isEmpty()) {
             mostrarToast("Completa todos los campos")
             return
         }
 
-        if (!correo.contains("@")) {
-            mostrarToast("Correo inválido")
+        // Validación del formato del correo
+        if (!correo.contains("@")|| !correo.contains(".")) {
+            mostrarToast("Formato de correo inválido")
             return
         }
 
+        // Validación de que la contraseña y repetir contraseña sean iguales
         if (contrasena != repetirContrasena) {
             mostrarToast("Las contraseñas no coinciden")
             return
         }
 
+        // tomamos el domionio del correo y lo guardamos en una variable
         val dominio = correo.substringAfterLast("@")
         val dominioConArroba = "@$dominio"
 
+        // Nos situamos en la rama de la BD de "empresas" (rama principal)
         val empresasRef = firestore.collection("empresas")
 
-        // Buscamos la empresa por dominio
+        // Validamos que el dominio de una empresa y el gominio introducido con el correo son iguales
         empresasRef.whereEqualTo("dominio", dominioConArroba).get()
             .addOnSuccessListener { empresasSnapshot ->
+                // Si no encuentra el domniono sale el siguiente mensaje
                 if (empresasSnapshot.isEmpty) {
                     mostrarToast("El dominio introducido no existe o es incorrecto")
                 } else {
+                    // En caso de encontrar el dominio, usará la rama de las empresas registradas para comprobar los siquientes datos
                     val empresaDoc = empresasSnapshot.documents[0]
+                    // Busca el cif en la BD
                     val empresaCif = empresaDoc.getString("cif") ?: ""
+                    // En caso de coincidir el etCif y el cif de la BD el empleado sería Jefe
                     val esJefe = cif.equals(empresaCif, ignoreCase = true)
-                    val cifFinal = empresaCif // se use o no, se guarda el cif correcto siempre
+                    // Se guarda el valor del cif sea o no correcto
+                    val cifFinal = empresaCif
+                    // Valida que la persona sea jefe y si el cif coincide con el de la BD
                     if (esJefe && cif != empresaCif) {
                         mostrarToast("El CIF no coincide con el dominio. Corrígelo para continuar.")
                         etCif.requestFocus()
@@ -107,8 +126,10 @@ class RegistroEmpleado : AppCompatActivity() {
                     auth.createUserWithEmailAndPassword(correo, contrasena)
                         .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
+                                // Se crea un id Personal para cada usuario
                                 val uid = auth.currentUser?.uid ?: ""
 
+                                // Se almacenan los datos de los editText en los campos definidos de la dataClass
                                 val nuevoUsuario = hashMapOf(
                                     "email" to correo,
                                     "cif" to cifFinal,
@@ -118,17 +139,21 @@ class RegistroEmpleado : AppCompatActivity() {
                                     "uid" to uid
                                 )
 
-                                // Guardamos datos extra en Firestore, colección "usuarios"
+                                // Guardamos datos extra en Firestore, colección "usuarios", tomamos como id el correo del usuario
                                 empresaDoc.reference.collection("usuarios").document(correo)
                                     .set(nuevoUsuario)
                                     .addOnSuccessListener {
+                                        // Si es jefe
                                         if (esJefe) {
                                             mostrarToast("Empleado registrado como Jefe")
+                                        // Si es empleado
                                         } else {
                                             mostrarToast("Empleado registrado")
                                         }
+                                        //
                                         limpiarCampos()
                                     }
+                                    // Añadimos excepción en caso de que haya un error al registrar al empleado
                                     .addOnFailureListener { e ->
                                         mostrarToast("Error guardando datos de usuario")
                                         e.printStackTrace()
@@ -139,16 +164,19 @@ class RegistroEmpleado : AppCompatActivity() {
                         }
                 }
             }
+            // Añadimos una excepción en caso de que el dominio del correo no corresponda con el dominio de una emresa de la BD
             .addOnFailureListener { e ->
                 mostrarToast("Error buscando empresa")
                 e.printStackTrace()
             }
     }
 
+    // Función que del toast que muestra qué tipo de empleado es
     private fun mostrarToast(mensaje: String) {
         Toast.makeText(this@RegistroEmpleado, mensaje, Toast.LENGTH_SHORT).show()
     }
 
+    // Función que pòne los campos en blanco
     private fun limpiarCampos() {
         etCorreo.text.clear()
         etContrasena.text.clear()
@@ -157,7 +185,8 @@ class RegistroEmpleado : AppCompatActivity() {
         etApellidos.text.clear()
         etCif.text.clear()
     }
-    // Flecha "Atrás"
+
+    // Se le añade la funcionalidad al botón de la toolbar
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             android.R.id.home -> {
